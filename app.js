6d1da8717e3a582557e58d687af1188c699db527
Denis Stryzhalov <imperor.oktov@gmail.com>
2021-09-09 13:44:29 +0300

    update api

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.use(cors())
 
-app.post('/data', (req, res) => {
+app.post('/lattlong', (req, res) => {
   console.log(req.body)
   axios.get(`http://www.metaweather.com/api/location/search?lattlong=${req.body.lattlong}`)
   .then(function (response) {
@@ -25,5 +25,41 @@ app.post('/data', (req, res) => {
   })
 
   });
+
+  app.post('/query', (req, res) => {
+    console.log(req.body)
+    axios.get(`http://www.metaweather.com/api/location/search?query=${req.body.query}`)
+    .then(function (response) {
+      // handle success
+      res.json(response.data)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      res.status(500)
+      res.json({
+          'error': "metaweather api error"
+      })
+    })
+  
+    });
+
+    app.post('/woeid', (req, res) => {
+      console.log(req.body)
+      axios.get(`http://www.metaweather.com/api/location/woeid/${req.body.woeid}`)
+      .then(function (response) {
+        // handle success
+        res.json(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+        res.status(500)
+        res.json({
+            'error': "metaweather api error"
+        })
+      })
+    
+      });
   const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => console.log('Listening on port 8080.'));
